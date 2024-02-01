90f69a9b33a6411d14f724c8af1239236ef31927
jinyong Kim <jyjy0509@naver.com>
2024-02-01 13:18:54 +0900

    commit

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application1.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application1.java
@@ -7,23 +7,7 @@ public class Application1 {
         A_for a = new A_for();
         //a.testSimpleForStatement();
         //a.testForExample1();
-        //a.tesForExample2();
-        //a.testForExample3();
-        //a.testForExample4();
-
-        A_nestedFor a2=new A_nestedFor();
-        //a2.printGugudanFromTwoToNine();
-        //a2.printStarInputRowTimes();
-
-        B_while b =new B_while();
-        //b.testSimpleWhileStatement();
-        //b.testWhileExample();
-        //b.testWhileExample3();
-
-        C_doWhile c =new C_doWhile();
-        //c.testSimpleDoWhileStatement();
-         c.testDoWhileExample1();
-
+        a.tesForExample2();
 
 
 
